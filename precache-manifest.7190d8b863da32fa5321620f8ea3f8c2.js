7f0bb44682111d7e7ea243c1d74bd81bac1c18df
jigyasa-ctrl <jigyasaupadhyay60@gmail.com>
2020-06-08 17:15:32 +0530

    Updates

---
+++ precache-manifest.7190d8b863da32fa5321620f8ea3f8c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc6d77c8e2fe483f0bf5ff6dde2ed2ff",
+    "url": "/ToDoList/index.html"
+  },
+  {
+    "revision": "b635a86658f46dc7bdd0",
+    "url": "/ToDoList/static/css/main.8e1d2d59.chunk.css"
+  },
+  {
+    "revision": "64307e05c8bec69fa50a",
+    "url": "/ToDoList/static/js/2.d42e7c4f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ToDoList/static/js/2.d42e7c4f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b635a86658f46dc7bdd0",
+    "url": "/ToDoList/static/js/main.4837d9f6.chunk.js"
+  },
+  {
+    "revision": "59b870f6fc92aef76d46",
+    "url": "/ToDoList/static/js/runtime-main.a4a9a566.js"
+  }
+]);
